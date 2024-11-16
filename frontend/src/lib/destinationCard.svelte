ef491816e5e43e8d0d5ba60110eec4785fbd1f5d
b-sharman <byron.n.sharman@gmail.com>
2024-11-16 17:58:00 -0600

    home page implementation

--- frontend/src/lib/destinationCard.svelte
+++ frontend/src/lib/destinationCard.svelte
@@ -4,7 +4,7 @@
   let { img, endDate, name }: {img: any, endDate: Date, name: string} = $props();
 </script>
 
-<Card {img}>
+<Card {img} class="max-w-[600px]">
   <h3 class="font-bold text-lg text-black">{name}</h3>
   <p class="text-sm text-gray-600">available until {endDate.toISOString().slice(0,10)}</p>
 </Card>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,4 +4,33 @@
   let tmpImage = 'rocks.jpg';
 </script>
 
-<DestinationCard img={tmpImage} endDate={new Date(Date.now())} name="Rocks" />
+<div class="flex flex-col items-center w-full py-24 bg-primary-300">
+  <h1 class="block m-2 text-6xl text-black font-bold">spontani</h1>
+  <p>unite through adventure</p>
+</div>
+
+<main class="mx-4">
+  <div class="my-12">
+    <header class="my-4">
+      <h2 class="my-2 mt-2 text-black text-3xl font-bold">current destinations</h2>
+      <p>where will you go today?</p>
+    </header>
+    <div class="grid grid-flow-row grid-cols-1 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))] gap-4 place-content-center">
+      {#each Array(10) as _}
+        <DestinationCard img={tmpImage} endDate={new Date(Date.now())} name="Rocks" />
+      {/each}
+    </div>
+  </div>
+
+  <div class="my-12">
+    <header class="my-4">
+      <h2 class="my-2 mt-2 text-black text-3xl font-bold">past destinations</h2>
+      <p>the deadline for going to these is over; now you can see everyone's pictures!</p>
+    </header>
+    <div class="grid grid-flow-row grid-cols-1 sm:grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] gap-4 place-content-center">
+      {#each Array(3) as _}
+        <DestinationCard img={tmpImage} endDate={new Date(Date.now())} name="Rocks" />
+      {/each}
+    </div>
+  </div>
+</main>
