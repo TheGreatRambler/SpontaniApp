f080a7d7386e50e6fe63bcafa2851bd64081f99d
b-sharman <byron.n.sharman@gmail.com>
2024-11-16 15:58:22 -0600

    add flowbite-svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,19 @@
+<script lang='ts'>
+  import { Button, Card } from 'flowbite-svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Card>
+  <h3>Location Name</h3>
+  <p>date</p>
+</Card>
+
+<Card>
+  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+  <Button class="w-fit">
+    Read more
+  </Button>
+</Card>
