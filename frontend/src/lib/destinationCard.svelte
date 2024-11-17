1ef259ddc7a8a5a22d7b9d78e0fa6f36d00b60c1
b-sharman <byron.n.sharman@gmail.com>
2024-11-16 19:17:04 -0600

    submit modal

--- frontend/src/lib/destinationCard.svelte
+++ frontend/src/lib/destinationCard.svelte
@@ -1,12 +1,16 @@
 <script lang='ts'>
   import { Card } from 'flowbite-svelte';
 
+  import SubmitModal from '$lib/submit.svelte';
+
   let { img, endDate, name }: {img: any, endDate: Date, name: string} = $props();
+  let openModal: boolean = $state(false);
 
   const tzOffset = new Date().getTimezoneOffset() * 60000;
 </script>
 
-<Card {img} class="max-w-[600px]">
+<Card onclick={() => openModal = true} {img} class="max-w-[600px] hover:bg-gray-200">
   <h3 class="font-bold text-lg text-black">{name.toLowerCase()}</h3>
   <p class="text-sm text-gray-600">available until {new Date(endDate - tzOffset).toISOString().slice(0,10)}</p>
 </Card>
+<SubmitModal {img} bind:openModal={openModal} {name} />
---
+++ frontend/src/lib/submit.svelte
@@ -0,0 +1,10 @@
+<script lang='ts'>
+  import { Modal } from 'flowbite-svelte';
+
+  let { img, openModal = $bindable(), name }: { img: any, openModal: boolean, name: string } = $props();
+</script>
+
+<Modal bind:open={openModal} outsideclose>
+  <h3 class="mb-4 text-xl font-bold text-black dark:text-white">recreate the picture of {name.toLowerCase()}</h3>
+  <p>align the camera as close as you can to the original and take a selfie!</p>
+</Modal>
