ffd401d92fbd6e5768b05180e30305d5a3908c5f
b-sharman <byron.n.sharman@gmail.com>
2024-11-16 18:03:19 -0600

    show date in local timezone

--- frontend/src/lib/destinationCard.svelte
+++ frontend/src/lib/destinationCard.svelte
@@ -2,9 +2,11 @@
   import { Card } from 'flowbite-svelte';
 
   let { img, endDate, name }: {img: any, endDate: Date, name: string} = $props();
+
+  const tzOffset = new Date().getTimezoneOffset() * 60000;
 </script>
 
 <Card {img} class="max-w-[600px]">
   <h3 class="font-bold text-lg text-black">{name}</h3>
-  <p class="text-sm text-gray-600">available until {endDate.toISOString().slice(0,10)}</p>
+  <p class="text-sm text-gray-600">available until {new Date(endDate - tzOffset).toISOString().slice(0,10)}</p>
 </Card>
