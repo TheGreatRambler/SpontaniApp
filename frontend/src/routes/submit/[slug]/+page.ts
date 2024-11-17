52994b13675a1a6f34fbfd78f864d27c6c1f3104
b-sharman <byron.n.sharman@gmail.com>
2024-11-17 06:24:53 -0600

    properly add destination data

--- frontend/src/routes/submit/[slug]/+page.ts
+++ frontend/src/routes/submit/[slug]/+page.ts
@@ -4,9 +4,7 @@ import type { Destination } from '$lib/destinationInterface.ts';
 
 export const load: PageLoad = async ({ fetch, params }) => {
   let data = {};
-  // data.destination = await (await fetch(`${import.meta.env.VITE_BASE_URL}/get?request_type=get_task&task_id=${params.slug}`)).json();
-  data.destination = await fetch(`${import.meta.env.VITE_BASE_URL}/get?request_type=get_task&id=${params.slug}`);
-  console.log(data.destination);
+  data.destination = await (await fetch(`${import.meta.env.VITE_BASE_URL}/get?request_type=get_task&id=${params.slug}`)).json();
   const imgObjects = await (await fetch(`${import.meta.env.VITE_BASE_URL}/get?request_type=get_images&task_id=${params.slug}`)).json();
   data.images = imgObjects.map((x) => {return {src: x.url};});
   return data;
