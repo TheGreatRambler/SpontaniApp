688deea10186d6e6e68fbc32c96aaba1723da49a
b-sharman <byron.n.sharman@gmail.com>
2024-11-17 08:56:05 -0600

    nicer css for submit

--- frontend/src/routes/submit/[slug]/+page.svelte
+++ frontend/src/routes/submit/[slug]/+page.svelte
@@ -8,16 +8,16 @@
 
 </script>
 
-<main class="mx-4">
-  <div class="my-5">
+<main class="flex justify-center items-center w-dvw h-dvh">
+  <div class="w-1/2 h-1/2">
     <header class="my-2 text-center">
-      <h2 class="my-2 mt-2 text-3xl font-bold">{data.destination.name}</h2>
+      <h2 class="my-2 mt-2 text-3xl font-bold">{data.destination.title}</h2>
       <p>{data.destination.description}</p>
     </header>
 
     <div class="flex justify-center">
       <div class="carousel-container w-full max-w-full space-y-4">
-        <Carousel images={data.images} let:Indicators let:Controls bind:index class="w-full [height:_500px_!important]">
+        <Carousel images={data.images} let:Indicators let:Controls bind:index class="w-full [height:_350px_!important]">
           <Controls />
           <Indicators />
         </Carousel>
@@ -26,6 +26,5 @@
         </div>
       </div>
     </div>
-
   </div>
 </main>
