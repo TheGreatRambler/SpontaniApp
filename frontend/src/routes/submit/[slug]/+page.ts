36016bd1a2adfe97b6035810f6700c6ca6d08d57
b-sharman <byron.n.sharman@gmail.com>
2024-11-17 06:12:53 -0600

    complete /submit route querying

--- frontend/src/routes/submit/[slug]/+page.ts
+++ frontend/src/routes/submit/[slug]/+page.ts
@@ -4,14 +4,9 @@ import type { Destination } from '$lib/destinationInterface.ts';
 
 export const load: PageLoad = async ({ fetch, params }) => {
   let data = {};
-  data.destination = {
-    description: 'foo',
-    endDate: 0,
-    img: '',
-    lat: 0.0,
-    lng: 0.0,
-    name: params.slug
-  };
+  // data.destination = await (await fetch(`${import.meta.env.VITE_BASE_URL}/get?request_type=get_task&task_id=${params.slug}`)).json();
+  data.destination = await fetch(`${import.meta.env.VITE_BASE_URL}/get?request_type=get_task&id=${params.slug}`);
+  console.log(data.destination);
   const imgObjects = await (await fetch(`${import.meta.env.VITE_BASE_URL}/get?request_type=get_images&task_id=${params.slug}`)).json();
   data.images = imgObjects.map((x) => {return {src: x.url};});
   return data;
