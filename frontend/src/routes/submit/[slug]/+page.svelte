f96bf820cf894334eee5f2f156bb2cbcd23e15cd
b-sharman <byron.n.sharman@gmail.com>
2024-11-17 06:08:29 -0600

    make /submit route partially functional

--- frontend/src/routes/submit/[slug]/+page.svelte
+++ frontend/src/routes/submit/[slug]/+page.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
+  import { Carousel, Thumbnails } from 'flowbite-svelte';
   import type { PageData } from './$types';
 
   let { data }: { data: PageData } = $props();
+
+  let index = $state(0);
+
 </script>
 
-<p>{data.name}</p>
-<p>replace this with the carousel stuff</p>
+<main class="mx-4">
+  <div class="my-5">
+    <header class="my-2 text-center">
+      <h2 class="my-2 mt-2 text-3xl font-bold">{data.destination.name}</h2>
+      <p>{data.destination.description}</p>
+    </header>
+
+    <div class="flex justify-center items-center">
+      <div class="carousel-container max-w-3xl space-y-4">
+        <Carousel images={data.images} let:Indicators let:Controls bind:index>
+          <Controls />
+          <Indicators />
+        </Carousel>
+        <Thumbnails images={data.images} bind:index />
+      </div>
+    </div>
+
+  </div>
+</main>
--- frontend/src/routes/submit/[slug]/+page.ts
+++ frontend/src/routes/submit/[slug]/+page.ts
@@ -2,14 +2,17 @@ import type { PageLoad } from './$types';
 
 import type { Destination } from '$lib/destinationInterface.ts';
 
-export const load: PageLoad = ({ params }) => {
-  const data: Destination = {
+export const load: PageLoad = async ({ fetch, params }) => {
+  let data = {};
+  data.destination = {
     description: 'foo',
     endDate: 0,
     img: '',
     lat: 0.0,
     lng: 0.0,
     name: params.slug
-  }
+  };
+  const imgObjects = await (await fetch(`${import.meta.env.VITE_BASE_URL}/get?request_type=get_images&task_id=${params.slug}`)).json();
+  data.images = imgObjects.map((x) => {return {src: x.url};});
   return data;
 };
