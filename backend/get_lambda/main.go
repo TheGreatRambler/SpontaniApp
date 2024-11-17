daef69a1c3905a4c8b75bd1d654086282ef00a50
TheGreatRambler <tgr@tgrcode.com>
2024-11-16 18:59:41 -0600

    Add default endpoint

--- backend/get_lambda/main.go
+++ backend/get_lambda/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,18 +14,27 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	"googlemaps.github.io/maps"
 )
 
-// Response structure for returning the nearby places
-type Response struct {
-	StatusCode int                       `json:"statusCode"`
-	Body       []maps.PlacesSearchResult `json:"body"`
-}
-
 var mapsClient *maps.Client
 var s3Client *s3.S3
+var dbConn *pgxpool.Pool
+
+type TaskRet struct {
+	Id           int     `json:"id"`
+	Title        string  `json:"title"`
+	Description  string  `json:"description"`
+	Lat          float64 `json:"lat"`
+	Lng          float64 `json:"lng"`
+	Uploaded     int64   `json:"uploaded"`
+	Start        int64   `json:"start"`
+	Stop         int64   `json:"stop"`
+	InitialImgId int     `json:"initial_img_id"`
+	Likes        int     `json:"likes"`
+}
 
 func init() {
 	// Load environment variables
@@ -46,49 +57,135 @@ func init() {
 			"",
 		),
 	})))
+
+	pgx_config, err := pgxpool.ParseConfig(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(fmt.Sprintf("Invalid databse URL: %v", os.Getenv("DATABASE_URL")))
+	}
+	pgx_config.MaxConns = 32
+	pgx_config.ConnConfig.ConnectTimeout = 10 * time.Second
+	dbConn, err = pgxpool.NewWithConfig(context.Background(), pgx_config)
+	if err != nil {
+		panic(fmt.Sprintf("Could not connect to database: %v", os.Getenv("DATABASE_URL")))
+	}
 }
 
-func handler(request events.APIGatewayProxyRequest) (Response, error) {
-	// Extract query parameters
-	latStr, latSet := request.QueryStringParameters["lat"]
-	lngStr, lngSet := request.QueryStringParameters["lng"]
-
-	if !latSet || !lngSet {
-		return Response{
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	request_type, exists := request.QueryStringParameters["request_type"]
+	if !exists {
+		return events.APIGatewayProxyResponse{
 			StatusCode: 400,
-			Body:       nil,
-		}, fmt.Errorf("location not provided")
+			Body:       "Missing required parameter: request_type",
+		}, nil
 	}
 
-	lat, err1 := strconv.ParseFloat(latStr, 64)
-	lng, err2 := strconv.ParseFloat(lngStr, 64)
-	if err1 != nil || err2 != nil {
-		return Response{
+	switch request_type {
+	case "get_nearby_recent_tasks":
+		lat_str, lat_exists := request.QueryStringParameters["lat"]
+		lng_str, lng_exists := request.QueryStringParameters["lng"]
+		if !lat_exists || !lng_exists {
+			return events.APIGatewayProxyResponse{
+				StatusCode: 400,
+				Body:       "Missing required parameters: lat, lng",
+			}, nil
+		}
+
+		lat, lat_err := strconv.ParseFloat(lat_str, 64)
+		lng, lng_err := strconv.ParseFloat(lng_str, 64)
+		if lat_err != nil || lng_err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: 400,
+				Body:       "Invalid required parameters: lat, lng",
+			}, nil
+		}
+
+		rows, err := dbConn.Query(context.Background(), `
+			SELECT id, title, description, lat, lng, uploaded,
+				start, stop, initial_img_id, likes,
+				point($1, $2) <@>  (point(lat, lng)::point) as distance
+				FROM task ORDER BY distance ASC
+		`, lat, lng)
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: 500,
+				Body:       fmt.Sprintf("Database error: %v", err),
+			}, nil
+		}
+		defer rows.Close()
+
+		tasks := []TaskRet{}
+		for rows.Next() {
+			var id int
+			var title string
+			var description string
+			var lat float64
+			var lng float64
+			var uploaded int64
+			var start int64
+			var stop int64
+			var initial_img_id int
+			var likes int
+			var distance float64
+			err := rows.Scan(&id, &title, &description, &lat, &lng, &uploaded, &start, &stop, &initial_img_id, &likes, &distance)
+			if err != nil {
+				return events.APIGatewayProxyResponse{
+					StatusCode: 500,
+					Body:       fmt.Sprintf("Database error: %v", err),
+				}, nil
+			}
+			events = append(events, TaskRet{
+				Id:           id,
+				Title:        title,
+				Description:  description,
+				Lat:          lat,
+				Lng:          lng,
+				Uploaded:     uploaded,
+				Start:        start,
+				Stop:         stop,
+				InitialImgId: initial_img_id,
+				Likes:        likes,
+			})
+		}
+
+		tasks_json, err := json.Marshal(tasks)
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: 500,
+				Body:       fmt.Sprintf("JSON marshalling error: %v", err),
+			}, nil
+		}
+
+		return events.APIGatewayProxyResponse{
+			StatusCode: 200,
+			Body:       string(tasks_json),
+		}, nil
+	default:
+		return events.APIGatewayProxyResponse{
 			StatusCode: 400,
-			Body:       nil,
-		}, fmt.Errorf("invalid location coordinates")
+			Body:       "Incorrect parameter value: request_type",
+		}, nil
 	}
 
-	// Perform Nearby Search
-	response, err := mapsClient.NearbySearch(context.Background(), &maps.NearbySearchRequest{
-		Location: &maps.LatLng{
-			Lat: lat,
-			Lng: lng,
-		},
-		Radius: uint(10), // Adjust radius as needed
-	})
-	if err != nil {
-		return Response{
-			StatusCode: 500,
-			Body:       nil,
-		}, fmt.Errorf("failed to perform Nearby Search: %v", err)
-	}
+	// // Perform Nearby Search
+	// response, err := mapsClient.NearbySearch(context.Background(), &maps.NearbySearchRequest{
+	// 	Location: &maps.LatLng{
+	// 		Lat: lat,
+	// 		Lng: lng,
+	// 	},
+	// 	Radius: uint(10), // Adjust radius as needed
+	// })
+	// if err != nil {
+	// 	return Response{
+	// 		StatusCode: 500,
+	// 		Body:       nil,
+	// 	}, fmt.Errorf("failed to perform Nearby Search: %v", err)
+	// }
 
-	// Return results as JSON
-	return Response{
-		StatusCode: 200,
-		Body:       response.Results,
-	}, nil
+	// // Return results as JSON
+	// return Response{
+	// 	StatusCode: 200,
+	// 	Body:       response.Results,
+	// }, nil
 }
 
 func main() {
