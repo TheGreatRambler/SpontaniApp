2366f036496774185964fd62a3236305da9d9163
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2024-11-17 01:40:37 -0600

    fixed form page

--- frontend/src/routes/form/+page.svelte
+++ frontend/src/routes/form/+page.svelte
@@ -26,25 +26,26 @@
     });
 </script>
 
-<div class="flex min-h-screen dark:bg-primary-300 ">
+<div class="flex min-h-screen dark:bg-primary-300">
     <div class="w-full flex flex-col space-y-6 px-8 py-4">
         <form class="flex flex-col space-y-6 w-full" action="/">
             <h3 class="text-xl font-medium text-gray-900 dark:text-white">Create a New Destination</h3>
             
-            <Label class="space-y-2 ">
+            <Label class="space-y-2">
                 <span>Title</span>
-                <Input type="text" name="title" placeholder="Day at the Beach" required />
+                <Input 
+                    type="text" name="title" placeholder="Day at the Beach" required class="bg-white dark:bg-primary-600 border dark-black dark:border-primary-500 text-gray-900 dark:text-white dark:placeholder-gray-400 rounded-lg p-2 focus:outline-none focus:border-primary-500 focus:ring-2 focus:ring-primary-300"/>
             </Label>
             
             <Label class="space-y-2">
                 <span>Description</span>
-                <textarea id="description" name="description" placeholder="Create sand castles at the beach." required class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:bg-primary-300 dark:focus:bg-primary-300 placeholder:text-sm"></textarea>
+                <textarea id="description" name="description" placeholder="Create sand castles at the beach." required class="block w-full p-2 text-gray-900 border dark:border-primary-500 rounded-lg bg-white dark:bg-primary-600 dark:text-white dark:placeholder-gray-400 focus:outline-none focus:border-primary-500 focus:ring-2 focus:ring-primary-300"></textarea>
             </Label>
 
             <Label class="space-y-2">
                 <span>Choose a Date</span>
                 <div class="mb-4 md:w-1/2">
-                    <Datepicker bind:value={selectedDate} />
+                    <Datepicker bind:value={selectedDate} class="bg-white dark:bg-primary-600 border border-primary-300 dark:border-primary-500" />
                 </div>
             </Label>
             
@@ -59,10 +60,10 @@
 
     <div class="w-full flex flex-col space-y-6 px-8 py-4">
         <form class="flex flex-col space-y-6 w-full" action="/">
-               
-        <Label class="space-y-2">
-            <h3 class="text-xl font-medium text-gray-900 dark:text-white">Location</h3>
-        </Label>
-        <div id="map" class="h-96 w-full"></div>
+                <Label class="space-y-2">
+                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">Location</h3>
+                </Label>
+                <div id="map" class="h-96 w-full"></div>
+        </form>
     </div>
 </div>
