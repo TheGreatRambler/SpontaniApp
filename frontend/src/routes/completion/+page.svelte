9672bad07b013361352e410c1284003d54914faa
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2024-11-17 00:03:40 -0600

    fixed formatting of task completion page

--- frontend/src/routes/completion/+page.svelte
+++ frontend/src/routes/completion/+page.svelte
@@ -1,47 +1,52 @@
 <script lang='ts'>
-    import { Carousel } from 'flowbite-svelte';
+    import { Carousel, Thumbnails } from 'flowbite-svelte';
   
     export const images = [
         {
-            alt: 'A picture of rocks',
+            alt: '1989',
             src: 'rocks.jpg',
             title: 'Rocks'
         },
         {
-            alt: 'A picture of snowy mountains',
+            alt: '1990',
             src: 'mountains.jpg',
             title: 'Mountains'
         },
         {
-            alt: 'A picture of a beach',
+            alt: '1991',
             src: 'beach.jpg',
             title: 'Beach'
         },
         {
-            alt: 'A picture of raindrops',
+            alt: '1992',
             src: 'raindrop.jpg',
-            title: 'Raindrop'
+            title: 'Raindrops'
         }
     ];
 
     let image = images[0];
+    let index = 0;
 </script>
 
 <main class="mx-4">
     <div class="my-5">
-        <header class="my-4">
+        <header class="my-2 text-center">
             <h2 class="my-2 mt-2 text-3xl font-bold">Day at the Beach</h2>
             <p>Create sand castles at the beach.</p>
         </header>
 
-        <div class="max-w-4xl space-y-4">
-            <Carousel {images} let:Indicators let:Controls on:change={({ detail }) => (image = detail)}>
-              <Controls />
-              <Indicators />
-            </Carousel>
-            <div class="rounded h-10 bg-gray-300 dark:bg-gray-700 dark:text-white p-2 my-2 text-center">
-                {image.alt}
+        <div class="flex justify-center items-center">
+            <div class="carousel-container max-w-3xl space-y-4">
+                <Carousel {images} let:Indicators let:Controls bind:index on:change={({ detail }) => (image = detail)}>
+                    <Controls />
+                    <Indicators />
+                </Carousel>
+                <Thumbnails {images} bind:index />
             </div>
-        </div> 
+        </div>
+
+        <div class=" dark:text-white p-2 my-2 text-center">
+            {images[index].alt}
+        </div>
     </div>
 </main>
